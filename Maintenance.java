ffab042addb5b9c237f4e33e579f4c4826c41b94
Luis Bernardo Monroy Jaramillo <111471014+luisbmonroyj@users.noreply.github.com>
2025-01-03 17:06:38 -0500

    Create Maintenance.java

---
+++ Maintenance.java
@@ -0,0 +1,63 @@
+import java.time.LocalDate;
+
+public class Maintenance {
+    private LocalDate  date;
+    private int id_bike;
+    private double odo;
+    private int id_service;
+    private String brand;
+    private String reference;
+    private double price;
+    private String description;
+    private String duration;
+    
+    public Maintenance() {
+    }
+    //constructor
+    public Maintenance(LocalDate fecha, int id_bike, double odometro, int id_service, String marca, String referencia, double precio, String descripcion, String duracion) {
+        this.date = fecha;
+        this.id_bike = id_bike; 
+        this.odo = odometro;
+        this.id_service = id_service;
+        this.brand = marca;
+        this.reference = referencia;
+        this.price = precio;
+        this.description = descripcion;
+        this.duration = duracion;
+    }
+
+    //getters
+    public LocalDate  getDate () {return date; }
+    public int getId_bike () {return id_bike; }
+    public double getOdo () {return odo; }
+    public int getId_service () {return id_service; }
+    public String getBrand () {return brand; }
+    public String getReference () {return reference; }
+    public double getPrice () {return price; }
+    public String getDescription () {return description; }
+    public String getDuration () {return duration; }
+     
+    //setters
+    public void setDate (LocalDate date) {this.date = date;}
+    public void setId_bike (int id_bike) {this.id_bike = id_bike; }
+    public void setOdo (double odo) {this.odo = odo;}
+    public void setId_service (int id_service) {this.id_service = id_service;}
+    public void setBrand (String brand) {this.brand = brand;}
+    public void setReference (String reference) {this.reference = reference;}
+    public void setPrice (double price) {this.price =price;}
+    public void setDescription (String description) {this.description = description;}
+    public void setDuration (String duration) {this.duration = duration;}
+    
+    public String getColumnList(){
+        return "date,id_bike,odo,id_service,brand,reference,price,description,duration";
+    }
+    public String toUpdateValues(double startingOdo, double startingTime){
+        return "date = '"+date+"', id_bike = "+Integer.toString(id_bike)+", odo = "+Double.toString(odo)+", id_service = "+Integer.toString(id_service)+
+            ", brand = '"+brand+"', reference = '"+reference+"', price = "+Double.toString(price)+", description = '"+description+"', duration = '"+duration+"'";    
+    }
+    //works for insert
+    public String toString(){
+        return date.toString()+","+Integer.toString(id_bike)+","+Double.toString(odo)+","+Integer.toString( id_service)+","+
+        brand+","+reference+","+Double.toString(price)+","+description+","+duration;
+    }
+}
